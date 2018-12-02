5ee2c40849d84394d6b23c9d9cdb707a8e55d472
Renato César <renatocx2014@gmail.com>
2018-12-02 21:35:50 -0200

    commit do que ja foi produzido do curso do branas

---
+++ js/controllers/listaTelefonicaCtrl.js
@@ -0,0 +1,54 @@
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $filter, contatosAPI, operadorasAPI, serialGenerator) {
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [];
+    $scope.operadoras = [];
+
+    var carregarContatos = function () {
+        contatosAPI.getContatos().then(function (response) {
+            $scope.contatos = response.data;
+        });
+    };
+
+    var carregarOperadoras = function () {
+        operadorasAPI.getOperadoras().then(function (response) {
+            $scope.operadoras = response.data;
+        });
+    }
+
+    $scope.adicionarContato = function (contato) {
+        contato.serial = serialGenerator.generate();
+        contato.data = new Date();
+        //$scope.contatos.push(angular.copy(contato))
+        contatosAPI.saveContato(contato).then(function (response) {
+            delete $scope.contato;
+            // resetando o estado do formulario para nunca tocado
+            $scope.contatoForm.$setPristine();
+            carregarContatos();
+        }, function (error) {
+            $scope.message = "Contato não salvo: " + response.data;
+        });
+    };
+
+    $scope.apagarContato = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado)
+                return contato;
+        });
+    };
+
+    //Se um dos contatos for true, a funcao some returna true
+    $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
+            return contato.selecionado;
+        });
+    };
+
+    $scope.ordenarPor = function (campo) {
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+
+    carregarContatos();
+    carregarOperadoras();
+
+});
